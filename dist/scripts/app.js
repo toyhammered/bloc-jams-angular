7bd47d8a1412c6e52128d51744a64258373ad1d3
toyhammered <daniel@rassiner.com>
2016-08-05 20:13:31 -0700

    completed assignment

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -16,6 +16,7 @@ angular.module('blocJams', ['ui.router']);
       })
       .state('album', {
         url: '/album',
+        controller: 'AlbumCtrl as album',
         templateUrl: '/templates/album.html'
       })
       .state('collection', {
